e0c3e802ea063cf1c27f710e6e88493ad38a3427
nick.rabinowitz <nick.rabinowitz@950c8186-9653-11de-9363-dbb1bd857576>
2009-09-16 20:55:39 +0000

    more modularizing

--- scripts/interface.js
+++ scripts/interface.js
@@ -3,150 +3,292 @@
  *---------------------------------------------------------------------------*/
 
 /**
- * @namespace   Namespace to hold functions dealing with the interface
+ * @class
+ * Geodia interface: currently featuring two side panels. This class deals with the
+ * layout, panels, and full-page interface stuff.
+ *
+ * @param {Geodia.Controller} controller    Associated controller
+ * @param {Object} options                  Container for optional params (probably to load state)
  */
-Geodia.Interface = { 
-    /** Whether the detail sidebar is open */
-    sbopen: false,
-    /** Whether the admin query panel sidebar is open */
-    adminopen: false,
-    /** Which side the sidebar is on */
-    sbside: 'r',
-    /** Which side the admin panel is on */
-    adminside: 'l'
-};
+Geodia.Interface = function(controller, options) {
 
-/**
- * Initialize the interface
- */
-Geodia.Interface.init = function() {
-    // set up panels
-    Geodia.Interface.resizePanels();
+    /** The associated controller */
+    this.controller = controller;
     
-    // set up resize function
-    // XXX: with more than one Geodia object, we'd need a chain here
-    window.timer = null;
-    window.onresize = function() {
-        if (window.timer === null) {
-            window.timer = window.setTimeout(function() {
-                window.timer = null;
-                // call twice to deal w/scrollbar
-                Geodia.Interface.resizePanels();
-                Geodia.Interface.resizePanels();
-                Geodia.controller.tm.timeline.layout();
-            }, 200);
+    var defaults = {
+        /** Whether the detail sidebar is open */
+        sbopen: false,
+        /** Whether the admin query panel sidebar is open */
+        adminopen: false,
+        /** Which side the sidebar is on */
+        sbside: 'r',
+        /** Which side the admin panel is on */
+        adminside: 'l'
+    };
+    
+    /** 
+     * Container for optional settings passed in the "options" parameter
+     * @type Object
+     */
+    this.opts = options = TimeMap.util.merge(options, defaults);
+    
+    /** The associated site list */
+    this.siteList = new Geodia.SiteList(this, this.controller);
+    
+    /** The associated admin panel */
+    this.adminPanel = new Geodia.AdminPanel(this, this.controller);
+    
+    
+    var ui = this;
+    
+    /**
+     * Initialize the interface
+     */
+    this.init = function() {
+        
+	    // handler to toggle sidebars
+	    $('div.toggler').click(function() {
+	        var id = $(this).parent('div').attr('id');
+	        ui.toggleSidebar(id);
+		    $(this).siblings().toggle();
+	    });
+        
+        // set up panels
+        this.resizePanels();
+        
+        // set up resize function
+        // XXX: with more than one Geodia object, we'd need a chain here
+        window.timer = null;
+        window.onresize = function() {
+            if (window.timer === null) {
+                window.timer = window.setTimeout(function() {
+                    window.timer = null;
+                    // call twice to deal w/scrollbar
+                    ui.resizePanels();
+                    ui.resizePanels();
+                }, 200);
+            }
         }
-    }
-};
-  
-/**
- * Resize the panels to fit the full window
- */
-Geodia.Interface.resizePanels = function() {
-    // get full screen size
-    var dh = $(window).height();
-    var dw = $(window).width();
-    var sbw = Geodia.Interface.sbopen ? 350 : 20;
-    var adw = Geodia.Interface.adminopen ? 350 : 20;
-    var hh = 31;
-    // size timemap and sidebar
-	var sbh = dh - hh;
-    $('#timemap').height(dh - hh)
-        .width(dw - sbw - adw);
-	$('ul.site_periods').height(sbh * .9);
-    $('#sidebar').height(dh - hh)
-        .width(sbw);
-    $('#admin_bar').height(dh - hh)
-        .width(adw);
-    // choose sidebar side
-    var border = '0px solid #CCCCCC';
-    if (Geodia.Interface.sbside == 'l') {
-        // left side
-        $('#sidebar')
-            .css('border-right', border);
-        $('#timemap')
-            .css('margin-right', sbw)
-            .css('left', sbw);
-    }
-   	else {
-        // right side
-        $('#admin_bar')
-            .css('border-right', border)
-            .css('left', 0);
-        $('#timemap')
-            .css('left', adw);
-        $('#sidebar')
-            .css('border-left', border)
-            .css('left', dw - sbw);
-    }
-
-    if (Geodia.controller.tm && Geodia.controller.tm.map) {
-        Geodia.controller.tm.map.checkResize();
-    }
-};
-
-/**
- * Add a site to the site list.
- *
- * @param {TimeMapItem} item    Item to add
- */
-Geodia.Interface.addToSiteList = function(item) {
-    var site_list = $('ul.site_list');
-    var site = $('<li>' + item.getTitle() + '</li>').appendTo(site_list);
-    $(site).data('site', item);
-    $(site).data('name', item.getTitle());
-    $(site).data('dataset', item.dataset);
-};
-
-/**
- * Clear site list
- */
-Geodia.Interface.clearSiteList = function() {
-    $('ul.site_list').empty();
-};
-
-/**
- * Show or hide loading animation
- *
- * @param {Boolean} loading     Whether loading is on or off     
- */
-Geodia.Interface.toggleLoading = function(loading) {
-    var loaderId = "ajax_loader";
-    if (loading) {
-        // check that the loader isn't already visible
-        if ($("#" + loaderId).size() < 1) {
-            // add loader image
-            $('<img id="' + loaderId + '" src="images/ajax-loader.gif"/>')
-                .insertAfter($('div.site_admin h1'));
+    };
+    
+    /**
+     * Resize the panels to fit the full window
+     */
+    this.resizePanels = function() {
+        // get full screen size
+        var dh = $(window).height();
+        var dw = $(window).width();
+        var sbw = options.sbopen ? 350 : 20;
+        var adw = options.adminopen ? 350 : 20;
+        var hh = 31;
+        // size timemap and sidebar
+	    var sbh = dh - hh;
+        $('#timemap').height(dh - hh)
+            .width(dw - sbw - adw);
+	    $('ul.site_periods').height(sbh * .9);
+        $('#sidebar').height(dh - hh)
+            .width(sbw);
+        $('#admin_bar').height(dh - hh)
+            .width(adw);
+        // choose sidebar side
+        var border = '0px solid #CCCCCC';
+        if (options.sbside == 'l') {
+            // left side
+            $('#sidebar')
+                .css('border-right', border);
+            $('#timemap')
+                .css('margin-right', sbw)
+                .css('left', sbw);
         }
-    }
-    else {
-        $("#" + loaderId).remove();
-    }
+       	else {
+            // right side
+            $('#admin_bar')
+                .css('border-right', border)
+                .css('left', 0);
+            $('#timemap')
+                .css('left', adw);
+            $('#sidebar')
+                .css('border-left', border)
+                .css('left', dw - sbw);
+        }
+
+        this.controller.checkResize();
+    };
+    
+    /**
+     * Add a site to the site list.
+     *
+     * @param {TimeMapItem} item    Item to add
+     */
+    this.addToSiteList = function(item) {
+        this.siteList.add(item);
+    };
+
+    /**
+     * Clear site list
+     */
+    this.clearSiteList = function() {
+        this.siteList.clear();
+    };
+    
+    /**
+     * Show or hide loading animation
+     *
+     * @param {Boolean} loading     Whether loading is on or off     
+     */
+    this.toggleLoading = function(loading) {
+        var loaderId = "ajax_loader";
+        if (loading) {
+            // check that the loader isn't already visible
+            if ($("#" + loaderId).size() < 1) {
+                // add loader image
+                $('<img id="' + loaderId + '" src="images/ajax-loader.gif"/>')
+                    .insertAfter($('div.site_admin h1'));
+            }
+        }
+        else {
+            $("#" + loaderId).remove();
+        }
+    };
+    
+    /**
+     * Toggle a sidebar open or closed
+     *
+     * @param {String} sb       sidebar to toggle ("sidebar" or "admin_bar")
+     */
+    this.toggleSidebar = function(sb) {
+        switch (sb) {
+            case 'sidebar':
+                options.sbopen = !options.sbopen;
+                break;
+            case 'admin_bar':
+                options.adminopen = !options.adminopen;
+                break;
+        }
+		ui.resizePanels();
+    };
 };
+
+/*----------------------------------------------------------------------------
+ * Site List
+ *---------------------------------------------------------------------------*/
+
+/**
+ * @class
+ * This class holds all functionality for managing the site list.
+ * Making a separate class here allows re-using the site list in different interfaces.
+ *
+ * @param {Geodia.Interface} ui             Associated interface
+ * @param {Geodia.Controller} controller    Associated controller
+ */
+Geodia.SiteList = function(ui, controller) {
+
+    /** The associated interface */
+    this.ui = ui;
+    
+    /** The associated controller */
+    this.controller = controller;
+
+    /**
+     * Add a site to the site list.
+     *
+     * @param {TimeMapItem} item    Item to add
+     */
+    this.add = function(item) {
+        var site_list = $('ul.site_list');
+        var site = $('<li>' + item.getTitle() + '</li>')
+            // add item reference
+            .data('site', item)
+            // add click handler
+            .click(function() {
+                item.openInfoWindow();
+            })
+            // add to site list
+            .appendTo(site_list);
+            
+        /* XXX: Not sure why clicking these items deletes the site now -
+        we probably want a separate function for that. Also
+        not sure about the performance consequences of the .live()
+        function - better to add the handler when a site is added to the list,
+        now in a separate function.
+            
+	    site.click(function(){
+		    var item = $(this).data('site');
+		    var dataset = $(this).data('dataset');
+		    var name = $(this).data('name');
+		    if (name == $('#site_title').text()){
+			    $('#site_title').empty();
+			    $('#site_description').empty();
+			    $('ul.site_periods').empty();
+		    }
+		    item.hide();
+		    item.hidePlacemark();
+		    if (dataset.items.length == 1){
+			    dataset.clear();
+			    if (name == $('#site_title').text()){
+				    $('#site_title').empty();
+				    $('#site_description').empty();
+				    $('ul.site_periods').empty();
+			    }
+			    delete dataset;
+		    }
+		    $(this).remove();
+		    delete item;
+            dataset.each(function(i) {
+                if (i.event){
+                    i.event._trackNum = null;
+                }
+            });
+            Geodia.controller.tm.timeline.layout();
+	    });
+	    */
+    };
+
+    /**
+     * Clear site list
+     */
+    this.clear = function() {
+        $('ul.site_list').empty();
+    };
+};
+
 
 /*----------------------------------------------------------------------------
  * Admin Panel
  *---------------------------------------------------------------------------*/
 
-$(document).ready(function(){
+/**
+ * @class
+ * This class holds all functionality for managing the admin panel
+ * Making a separate class here allows re-using the panel in different interfaces.
+ *
+ * @param {Geodia.Interface} ui             Associated interface
+ * @param {Geodia.Controller} controller    Associated controller
+ */
+Geodia.AdminPanel = function(ui, controller) {
 
-	$('#clear_all').click(function(){
-		Geodia.controller.clear();
+    /** The associated interface */
+    this.ui = ui;
+    
+    /** The associated controller */
+    this.controller = controller;
+    
+    /**
+     * Clear all ui elements and all data.
+     */
+    this.clear = function() {
+		controller.clear();
 		$('ul.site_list').empty();
 		$('#site_title').empty();
 		$('#site_description').empty();
 		$('ul.site_periods').empty();
 		return false;
-	});
-
-	$('#show').click(function(){
-		return false;
-	});
-
-	$('input[type="checkbox"]').attr('checked',false);
-	$('input[type="text"]').val('');
-	$('input[type="checkbox"]').click(function() {
+	};
+	
+	/**
+	 * Load data based on facets
+	 */
+	this.loadFacets = function() {
 		var cultures = [], regions = [];
         $('#culture_list')
             .children('input[type="checkbox"]:checked')
@@ -158,68 +300,30 @@ $(document).ready(function(){
             .each(function(i, el){
                 regions.push($(el).val());			
             });
-        Geodia.controller.loadFacets(cultures, regions);
-    });
-
-	$('#search_button').click(function(){
-        var term = $(this).prev('input').val();
-        Geodia.controller.loadSearch(term);
-	});
-
-	//toggle sidebars
-	$('div.toggler').click(function(){
-			var id = $(this).parent('div').attr('id');
-			if (id == 'sidebar'){
-				if ($(this).parent('div').width() > 20){
-					Geodia.Interface.sbopen = false;
-				}
-				else{
-					Geodia.Interface.sbopen = true;
-				}
-			}
-			if (id == 'admin_bar'){
-				if ($(this).parent('div').width() > 20){
-					Geodia.Interface.adminopen = false;
-				}
-				else{
-					Geodia.Interface.adminopen = true;
-				}
-			}
-			$(this).siblings().toggle();
-			Geodia.Interface.resizePanels();
-	});
+        controller.loadFacets(cultures, regions);
+    };
+	
+	/**
+	 * Load data based on search query
+	 */
+	this.loadSearch = function(){
+        var term = $('#search_text').val();
+        controller.loadSearch(term);
+	};
     
-	$('ul.site_list li').live('click',function(){
-		var item = $(this).data('site');
-		var dataset = $(this).data('dataset');
-		var name = $(this).data('name');
-		if (name == $('#site_title').text()){
-			$('#site_title').empty();
-			$('#site_description').empty();
-			$('ul.site_periods').empty();
-		}
-		item.hide();
-		item.hidePlacemark();
-		if (dataset.items.length == 1){
-			dataset.clear();
-			if (name == $('#site_title').text()){
-				$('#site_title').empty();
-				$('#site_description').empty();
-				$('ul.site_periods').empty();
-			}
-			delete dataset;
-		}
-		$(this).remove();
-		delete item;
-        dataset.each(function(i) {
-            if (i.event){
-                i.event._trackNum = null;
-            }
-        });
-        Geodia.controller.tm.timeline.layout();
-	});
+    // Initialize panel
+        
+    // clear panel
+    $('#admin_bar input[type="checkbox"]').attr('checked',false);
+    $('#search_text').val('');
+    
+    // set handlers
+    $('#clear_all').click(this.clear);
+    $('#culture_list input[type="checkbox"]').click(this.loadFacets);
+    $('#region_list input[type="checkbox"]').click(this.loadFacets);
+    $('#search_button').click(this.loadSearch);
 
-});
+};
 
 /*----------------------------------------------------------------------------
  * Sidebar Panel
@@ -227,6 +331,7 @@ $(document).ready(function(){
 
 /**
  * Initialize images for a selected site
+ * XXX: This needs some love - conflates the sidebar set up with the image viewer
  */
 Geodia.Interface.initImageViewer = function(current, periods, site_name, description){
         
